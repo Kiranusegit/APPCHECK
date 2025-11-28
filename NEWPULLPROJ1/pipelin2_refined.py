38d8f93dbe4547582fea7d912915cd40433493cb
Streamlit Pipeline Designer <kiranusegit>
2025-11-28 11:41:50 -0500

    Update pipeline code: PIPELIN2 (Refined)

---
+++ NEWPULLPROJ1/pipelin2_refined.py
@@ -0,0 +1,41 @@
+Here is the refined Python code:
+```python
+import sys
+from pyspark.sql import SparkSession
+
+spark = SparkSession \
+    .builder \
+    .appName("PIPELIN2") \
+    .config("spark.some.config.option", "some-value") \
+    .getOrCreate()
+
+# Read data from JDBC connection
+read_data = spark.read \
+    .format("jdbc") \
+    .option("url", "TEST") \
+    .option("dbtable", "TEST") \
+    .option("user", "TEST") \
+    .option("password", "TEST") \
+    .load()
+
+# Filter data based on conditions
+filter_1 = read_data.filter(condition)
+filter_2 = filter_1.filter(condition10)
+
+# Aggregate data by group by columns and perform aggregation
+aggregation_3 = filter_2.groupBy("col1").agg(condition12)
+
+# Join data from two sources based on join condition
+join_5 = read_data \
+    .join(aggregation_3, on=["Join Condition111sadsasdasd"])
+
+# Filter data based on conditions after joining
+filter_7 = join_5.filter(condition100)
+filter_8 = filter_7.filter(lambda r: r["filter condition"])
+
+# Write filtered data to a file in JSON format
+write_data_6 = filter_8.write \
+    .format("json") \
+    .option("mode", "overwrite") \
+    .option("path", "path target") \
+    .save()
